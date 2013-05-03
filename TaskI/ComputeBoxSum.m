b9ffbbdb42b6851847f3412d6e8fff863b07a347
Romain Pomier <romain.pomier@gmail.com>
2013-05-03 14:41:50 +0200

    Simplify ComputeBoxSum

--- TaskI/ComputeBoxSum.m
+++ TaskI/ComputeBoxSum.m
@@ -1,20 +1,15 @@
 function A = ComputeBoxSum(ii_im, x, y, w, h)
-% (x-1) !=0
-if x ==1
-    % (y-1) != 0
-    if y ==1
-        % 4
-        A = ii_im(y+h-1,x+w-1);
-    else 
-        % 4 - 2
-        A = ii_im(y+h-1,x+w-1) -ii_im(y-1,x+w-1);
-    end    
-else if y ==1
-    % 4 - 3
-    A = ii_im(y+h-1,x+w-1) -ii_im(y+h-1,x-1);
-else
-    % 4+1 - 2-3
-    A = ii_im(y+h-1,x+w-1) + ii_im(y-1,x-1) - ii_im(y-1,x+w-1)-ii_im(y+h-1,x-1);
-    end
+
+A = ii_im(y+h-1,x+w-1);
+
+if x ~= 1
+    A = A - ii_im(y+h-1,x-1);
 end
+if y ~= 1
+    A = A - ii_im(y-1,x+w-1);
+end
+if x ~= 1 && y ~= 1
+    A = A + ii_im(y-1,x-1);
+end
+
 end
