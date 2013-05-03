c91ed2badada7fb836533259e8f670b726830e4a
Romain Pomier <romain.pomier@gmail.com>
2013-05-03 15:27:53 +0200

    Clean EnumAllFeatures

--- TaskI/EnumAllFeatures.m
+++ TaskI/EnumAllFeatures.m
@@ -1,12 +1,7 @@
-function all_ftypes = EnumAllFeatures(W, H,ftype)
+function all_ftypes = EnumAllFeatures(W, H)
 all_ftypes = zeros(50000,5);
 index = 1;
 
-% ----------------------------
-if (ftype == 1)
-% ----------------------------
-% Why start in 2? ******
-% Width is constant.
 for w = 1:W-2
     % Height is splitted in 2 parts.
     for h = 1:floor(H/2)-2        
@@ -14,41 +9,29 @@ for w = 1:W-2
             % y starts in 2 and it stops where h can no longer be splitted
             % in two            
             for y = 2:H-2*h
-                % Put the instance
-                all_ftypes(index,:) = [ftype,x,y,w,h];
-                % Add one to the index.
+                all_ftypes(index,:) = [1,x,y,w,h];
                 index=index+1;
             end
         end        
     end
 end
 
-% ----------------------------
-else if ftype == 2
-% ----------------------------
-% Why start in 2?
+
 % Width splits in 2 parts.
 for w = 1:floor(W/2)-2
-    % Height is constant.
     for h = 1:H-2
         % x starts in 2 and it stops where w can no longer be splitted
         % in two
         for x = 2:W-2*w
-            % Same for y but only where h fits.
             for y = 2:H-h
-                % Put the instance
-                all_ftypes(index,:) = [ftype,x,y,w,h];
-                % Add one to the index.
+                all_ftypes(index,:) = [2,x,y,w,h];
                 index=index+1;
             end
         end        
     end
 end
-    
-% ----------------------------
-    else if ftype == 3
-% ----------------------------
-% Why start in 2?
+
+
 % Width splits in 2 parts.
 for w = 1:floor(W/3)-2
     % Height is constant.
@@ -58,20 +41,13 @@ for w = 1:floor(W/3)-2
         for x = 2:W-3*w
             % Same for y but only where h fits.
             for y = 2:H-h
-                % Put the instance
-                all_ftypes(index,:) = [ftype,x,y,w,h];
-                % Add one to the index.
+                all_ftypes(index,:) = [3,x,y,w,h];
                 index=index+1;
             end
         end        
     end
 end
 
-% ----------------------------
-        else if ftype == 4
-% ----------------------------
-% Why start in 2?
-% Width splits in 2 parts.
 for w = 1:floor(W/2)-2
     % Height is constant.
     for h = 1:floor(H/2)-2
@@ -80,19 +56,13 @@ for w = 1:floor(W/2)-2
         for x = 2:W-2*w
             % Same for y.
             for y = 2:H-2*h
-                % Put the instance
-                all_ftypes(index,:) = [ftype,x,y,w,h];
-                % Add one to the index.
+                all_ftypes(index,:) = [4,x,y,w,h];
                 index=index+1;
             end
         end        
     end
 end
-            end
-        end
-    end
-end
-%b= all_ftypes(find(all_ftypes(1) ~= 0));
+
 % Remove one to the index as it will have +1 at the last instance.
 all_ftypes = all_ftypes(1:index-1,:);
 end
