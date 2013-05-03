c757df3f981dd7d7ab7fcfbd96245659b539777a
Romain Pomier <romain.pomier@gmail.com>
2013-05-03 16:31:28 +0200

    Clean VecBoxSum

--- TaskI/VecBoxSum.m
+++ TaskI/VecBoxSum.m
@@ -1,13 +1,11 @@
 function b_vec = VecBoxSum(x, y, w, h, W, H)    
-    b_vec = zeros(H,W);    
-    % First point (4)
-    b_vec( y+h-1,x+w-1 ) = 1;
-    % Second point (1)
-    b_vec( y-1,x-1 ) =1;
-    % Third point (-2)
-    b_vec( y-1,x+w-1 ) = -1;
-    % Forth point (-3)
-    b_vec( y+h-1,x-1 ) = -1;
-    % Transform it!
+    b_vec = zeros(H,W); 
+    
+    b_vec(y+h-1, x+w-1) = 1;
+    b_vec(y-1, x-1) = 1;
+    b_vec(y-1, x+w-1) = -1;
+    b_vec(y+h-1, x-1) = -1;
+    
+    % Make it a vector
     b_vec = b_vec(:);
 end
