89af34b4e26671289e08d9aae3b6cc6c89adb224
Romain Pomier <romain.pomier@gmail.com>
2013-05-03 17:06:44 +0200

    VecComputeFeature sanity check

--- TaskI/VecComputeFeature.m
+++ TaskI/VecComputeFeature.m
@@ -1,5 +1,6 @@
 function fs = VecComputeFeature(ii_ims, ftype_vec)
-fs = zeros(size(ii_ims, 1),1);
+n = size(ii_ims, 1);
+fs = zeros(n,1);
 
 for i = 1:n
     ii_im = reshape(ii_ims(i,:,:), size(ii_ims(i,:,:),2),size(ii_ims(i,:,:),3));
