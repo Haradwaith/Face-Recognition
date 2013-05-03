6afd2fa2586553de8a0a4c965a2da212001ff7da
Romain Pomier <romain.pomier@gmail.com>
2013-05-03 16:55:48 +0200

    Clean VecComputeFeature

--- TaskI/VecComputeFeature.m
+++ TaskI/VecComputeFeature.m
@@ -1,15 +1,9 @@
 function fs = VecComputeFeature(ii_ims, ftype_vec)
-n = length(ii_ims);
-fs = zeros(n,1);
+fs = zeros(size(ii_ims, 1),1);
 
-% ----------------------------------
-    % Doesnt matter which case is it
-    % It is always the same.
-% ----------------------------------
-    for i = 1:n
-        % Reshape the ii_ims:i
-        ii_im = reshape(ii_ims(i,:,:), size(ii_ims(i,:,:),2),size(ii_ims(i,:,:),3));
-        % Compute the ftype feature        
-        fs(i) = ii_im(:)'*ftype_vec;
-    end    
+for i = 1:n
+    ii_im = reshape(ii_ims(i,:,:), size(ii_ims(i,:,:),2),size(ii_ims(i,:,:),3));
+    fs(i) = ii_im(:)'*ftype_vec;
+end
+
 end
