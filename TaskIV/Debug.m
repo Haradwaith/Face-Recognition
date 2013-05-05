449752f84c294d499c81281ee22519819b80caba
Bertrand Mermet <bertrandmermet@gmail.com>
2013-05-05 15:18:06 +0200

    Attempt to solve 5.1

--- TaskIV/Debug.m
+++ TaskIV/Debug.m
@@ -1,28 +1,81 @@
 %Debug%
-im = '../TestImages/one_chris.png';
-addpath('../TaskIII');
-
+addpath('TaskIII');
+addpath('TaskII');
+addpath('TaskI');
 % Program 20
-Cparams = load('../TaskII/Cparams.mat');
-Cparams = Cparams.Cparams;
-Fdata = load('FaceData.mat');
-NFdata = load('NonFaceData.mat');
-FTdata = load('FeaturesToUse.mat');
+load('Cparams.mat');
+% Fdata = load('FaceData.mat');
+% NFdata = load('NonFaceData.mat');
+% FTdata = load('FeaturesToUse.mat');
 
-I = imread(im);
-imshow(I)
-I = rgb2gray(I);
-I = double(I);
+% ----------------------------------
+% One file test
+% ----------------------------------
+im_fname = 'TrainingImages/FACES/face00001.bmp';
+[im, ii_im] = LoadIm(im_fname);
+% Apply detector
+ApplyDetector(Cparams, ii_im);
+dets = ScanImageFixedSize(Cparams,im);
+DisplayDetections(im_fname, dets);
+colormap gray;
 
-% Illummination invariance
-u = mean(I(:));
-o = std(I(:));
-if (o~=0)
-    im = (I-u)/o;
-    norm_image= im;
-else
-    im = I;
-end
+% % % ----------------------------------
+% % % Program 22 Debug
+% % % ----------------------------------
+% % 
+% % More robust check!
+% 
+% % Get the test images.
+% Fdata = load('FaceData.mat');
+% face_fnames = dir(Fdata.dirname);
+% index = 3:length(face_fnames);
+% face_test_index = setdiff(index,Fdata.fnums);
+% 
+% % Store the values of scores.
+% scores_faces = zeros(1,length(face_test_index));
+% new_scores_faces = zeros(1,length(face_test_index));
+% 
+% % To access the test images we do
+% for i=1:length(face_test_index)
+%     % Choose filename
+%     im_fname = [Fdata.dirname, face_fnames(face_test_index(i)).name];
+%     % ----------------
+%     % Load data
+%     % ----------------
+%     [im, ii_im] = LoadIm(im_fname);
+%     
+%     % Apply detector
+%     scores_faces(i) = ApplyDetector(Cparams, ii_im);
+%     new_scores_faces(i) = ScanImageFixedSize(Cparams,ii_im);
+%     
+% end
+% sum(abs(scores_faces-new_scores_faces))
+ 
+ 
+%% ----------------------------------
+%% Program 23 Debug
+%% ----------------------------------
+%% Test ScanImageFixedSize
+path = 'TestImages/one_chris.png';
+ 
+% Load the image
+[im, ii_im] = LoadIm(path);
+ 
+% % Without pruning
+dets = ScanImageFixedSize(Cparams, im);
+DisplayDetections(path, dets);
 
-% Integral Image.
-ii_im = cumsum(cumsum(im,2));
+%% Display the detected face with pruning
+%fdets = PruneDetections(dets);
+%DisplayDetections(path, fdets);
+%
+% % Test ScanImageOverScale: Fucked!!!
+% im = 'TestImages/big_one_chris.png';
+% min_s = 0.1;
+% max_s = 1.0;
+% step_s = .06;
+%
+% dets = ScanImageOverScale(Cparams, im, min_s, max_s, step_s);
+% DisplayDetections(im, dets);
+%
+%
--- TaskIV/ScanImageFixedSize.m
+++ TaskIV/ScanImageFixedSize.m
@@ -1,53 +1,31 @@
 function dets = ScanImageFixedSize(Cparams, im)
-%Read the image    
-I = imread(im);
 
-% Do image procesing
-if size(im,3) == 3
-    I = rgb2gray(I);
+if size(im, 3) > 1
+    im = im(:,:,1) + im(:,:,2) + im(:,:, 3);
 end
 
-% Illummination invariance
-u = mean(I(:));
-o = std(I(:));
-if (o~=0)
-    im = (I-u)/o;
-else
-    im = I;
-end
-
-% Integral Image.
-ii_im = cumsum(cumsum(im,2));
-
+X = size(im,1);
+Y = size(im,2);
 L = 19;
-[Y,X]=size(im);
-% score vector
-sc = zeros(1,4);
+squared_im = im .* im;
+ii_im = cumsum(cumsum(im,2));
 dets = [];
-% Square the ii
-squared = ii_im.*ii_im;
-for x = 1:X-L
-    for y = 1:Y-L
-        % -------------------------------------
-        % Normalize the box
-        % -------------------------------------
-        patch = ii_im(y:y+L-1,x:x+L-1);
-        % Compute mean
-        mu = (1/L^2)*sum(sum(patch));        
-        sq = squared(y:y+L-1,x:x+L-1);
-        % Compute sigma
-        sigma = sqrt((1/ (L^2-1))*(sum(sum(sq)) - L^2*mu^2)); 
-        % -------------------------------------
-        % Apply detector
-        % Error here!!!!!!!!!
-        sc = ApplyDetector(Cparams,(1/sigma)*patch);
-        % Is it a face?
-        if (sc>0)%Cparams.thresh)
-          % Keep it as a face  
-          dets = [dets;sc];%[x,y,L,L]];
-        end        
+
+for x = 1:X+1-L
+    for y = 1:Y+1-L
+
+        % size(squared_im);
+        mu = (1/L^2)*sum(sum(im(x:x+L-1,y:y+L-1)));
+        sigma = sqrt((1/(L^2 - 1))*(sum(sum(squared_im(x:x+L-1,y:y+L-1))) -L^2 *mu^2));
+
+        sc = ApplyDetectorAdapted(Cparams, ii_im(x:x+L-1,y:y+L-1), sigma, mu, L);
+
+        % keep detected faces
+        if sc > Cparams.thresh
+            dets = [dets; [x,y,L,L]];
+        end
     end
 end
-% x = 22, y = 25;
 
 end
+
