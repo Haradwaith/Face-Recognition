13fa481eab5f34e2909b17585dbbbe9036484cc0
Romain Pomier <romain.pomier@gmail.com>
2013-05-05 16:19:05 +0200

    Clean Debug

--- TaskIV/Debug.m
+++ TaskIV/Debug.m
@@ -1,16 +1,15 @@
-%Debug%
+%% Init
+
 addpath('TaskIII');
 addpath('TaskII');
 addpath('TaskI');
-% Program 20
 load('Cparams.mat');
 % Fdata = load('FaceData.mat');
 % NFdata = load('NonFaceData.mat');
 % FTdata = load('FeaturesToUse.mat');
 
-% ----------------------------------
-% One file test
-% ----------------------------------
+%% One file test
+
 im_fname = 'TrainingImages/FACES/face00001.bmp';
 [im, ii_im] = LoadIm(im_fname);
 % Apply detector
@@ -18,43 +17,10 @@ ApplyDetector(Cparams, ii_im);
 dets = ScanImageFixedSize(Cparams,im);
 DisplayDetections(im_fname, dets);
 colormap gray;
+ 
 
-% % % ----------------------------------
-% % % Program 22 Debug
-% % % ----------------------------------
-% % 
-% % More robust check!
-% 
-% % Get the test images.
-% Fdata = load('FaceData.mat');
-% face_fnames = dir(Fdata.dirname);
-% index = 3:length(face_fnames);
-% face_test_index = setdiff(index,Fdata.fnums);
-% 
-% % Store the values of scores.
-% scores_faces = zeros(1,length(face_test_index));
-% new_scores_faces = zeros(1,length(face_test_index));
-% 
-% % To access the test images we do
-% for i=1:length(face_test_index)
-%     % Choose filename
-%     im_fname = [Fdata.dirname, face_fnames(face_test_index(i)).name];
-%     % ----------------
-%     % Load data
-%     % ----------------
-%     [im, ii_im] = LoadIm(im_fname);
-%     
-%     % Apply detector
-%     scores_faces(i) = ApplyDetector(Cparams, ii_im);
-%     new_scores_faces(i) = ScanImageFixedSize(Cparams,ii_im);
-%     
-% end
-% sum(abs(scores_faces-new_scores_faces))
- 
- 
-%% ----------------------------------
 %% Program 23 Debug
-%% ----------------------------------
+
 %% Test ScanImageFixedSize
 path = 'TestImages/one_chris.png';
  
