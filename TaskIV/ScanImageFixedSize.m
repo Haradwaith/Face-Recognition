c979e6dc4f153e8f793de63ed10edd1095b7b1c1
Romain Pomier <romain.pomier@gmail.com>
2013-05-05 15:57:55 +0200

    Use integral images to compute mean and variance

--- TaskIV/ScanImageFixedSize.m
+++ TaskIV/ScanImageFixedSize.m
@@ -1,7 +1,8 @@
 function dets = ScanImageFixedSize(Cparams, im)
 
 if size(im, 3) > 1
-    im = im(:,:,1) + im(:,:,2) + im(:,:, 3);
+    %im = im(:,:,1) + im(:,:,2) + im(:,:, 3);
+    im = rgb2gray(im);
 end
 
 X = size(im,1);
@@ -9,14 +10,13 @@ Y = size(im,2);
 L = 19;
 squared_im = im .* im;
 ii_im = cumsum(cumsum(im,2));
+squared_ii_im = cumsum(cumsum(squared_im,2));
 dets = [];
 
 for x = 1:X+1-L
     for y = 1:Y+1-L
-
-        % size(squared_im);
-        mu = (1/L^2)*sum(sum(im(x:x+L-1,y:y+L-1)));
-        sigma = sqrt((1/(L^2 - 1))*(sum(sum(squared_im(x:x+L-1,y:y+L-1))) -L^2 *mu^2));
+        mu = ComputeBoxSum(ii_im, x, y, L, L)/(L^2);
+        sigma = sqrt((1/(L^2 - 1))*(ComputeBoxSum(squared_ii_im, x, y, L, L) -L^2 *mu^2));
 
         sc = ApplyDetectorAdapted(Cparams, ii_im(x:x+L-1,y:y+L-1), sigma, mu, L);
 
